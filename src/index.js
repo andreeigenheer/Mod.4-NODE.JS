cb7efdbf177c9e111010e7d1e07d0f41736141f4
Lucas Vinicius Ferreira <lucasvinicius.ferreira@outlook.com>
2021-07-26 22:30:59 -0300

    Subindo arquivos atualizados
    Revisão dos arquivos(primeira versão funcional, revisar update).

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
-//teste de commit
+const express = require("express");
+const cors = require("cors");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const handlebars = require('express-handlebars')
+//Configura porta do servidor
+const {APP_PORT} = require("./Config/appConfig");
 
-const app = express()
+//importa classes que controlam as requisições de Alunos e Professores
+const ProfessorController = require("./Controller/profController");
+const AlunoController = require("./Controller/alunosController");
 
-app.listen(3000, function(req, res) {
-    console.log('Servidor está rodando');
+//importa conexão com a base de dados
+const database = require("./infra/sqlite-db");
+
+//Importa objetos de acesso aos dados
+const ProfessorDAO = require("./DAO/profDao");
+const AlunosDAO = require("./DAO/alunosDao");
+
+
+//instacia DAO para os controllers
+const profDAO = new ProfessorDAO(database);
+const alunosDAO = new AlunosDAO(database);
+
+// instancia controllers
+const professorController = new ProfessorController(profDAO);
+const alunoController = new AlunoController(alunosDAO);
+
+//Cria aplicação
+const app = express();
+
+//Invoca o cors para permitir requisições
+app.use(cors());
+
+//Middleware para fazer Json Parse nas requisições
+app.use(express.json());
+
+//Rota de teste
+
+app.get("/",function(req, res){
+    res.send({message:"Rota inicial"})
 });
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/projeto.html")
-});
+//Define rotas de professor
+app.get("/professor/:nome", professorController.visualizaUm);
+app.get("/professor",professorController.visualizaTodos);
+app.post("/professor",professorController.insere);
+app.put("/professor/:nome", professorController.atualiza);
+app.delete("/professor/:nome",professorController.remove);
+//Define rotas de alunosDAO
+app.get("/aluno/:nome", alunoController.visualizaUm);
+app.get("/aluno", alunoController.visualizaTodos);
+app.post("/aluno", alunoController.insere);
+app.put("/aluno/:nome", alunoController.atualiza);
+app.delete("/aluno/:nome", alunoController.remove);
 
-//Iniciando o server
+//Servidor rodando na porta 3000
+app.listen(APP_PORT, ()=> {
+    console.log(`Aplicação escutando a porta ${APP_PORT}`);
+});
