2ff1372ac82c50024e610039d38eee09be8ef0b6
Renata Hortense <81193034+renatahortense@users.noreply.github.com>
2021-07-28 15:46:08 -0300

    Alteração Async

--- src/Controller/profController.js
+++ src/Controller/profController.js
@@ -7,82 +7,65 @@ class ProfessorController {
   constructor(conecta) {
     this.conecta = conecta;
   }
-  //Insira a função que exibe um professor (método GET)
-  visualizaUm = (req, res) => {
+  //Função que exibe um professor (método GET)
+  visualizaUm = async (req, res) => {
     const nome = req.params.nome;
+    try {
+      const exibeProf = await this.conecta.visualizaUmProf(nome);
+      res.send(exibeProf);
+    } catch (err) {
+      res.send(err);
+    }
+  };
+  //Função que exibe todos os professores (método GET)
+  visualizaTodos = async (req, res) => {
+    try {
+      const exibeProf = await this.conecta.visualizaTodosProfs();
+      res.send(exibeProf);
+    } catch (err) {
+      res.send(err);
+    }
+  };
+  //Função que insere um professor (método POST)
+  insere = async (req, res) => {
+    const { nome, disciplina, email } = req.body;
+    const professor = new Professor(nome, disciplina, email);
 
-    this.conecta
-      .visualizaUmProf(nome)
-      .then((professor) => {
-        res.send(professor);
-      })
-      .catch((err) => {
-        throw err;
-      });
+    try {
+      const insereProf = await this.conecta.insereProf(professor);
+      res.send(insereProf);
+    } catch (err) {
+      res.send(err);
+    }
   };
-  //Insira a função que exibe todos os professores (método GET)
-  visualizaTodos = (req, res) => {
-    this.conecta
-      .visualizaTodosProfs()
-      .then((nome) => {
-        res.send(nome);
-      })
-      .catch((err) => {
-        throw err;
-      });
-  };
-  //Insira a função que insere um professor (método POST)
-  insere = (req, res) => {
-    const {nome,disciplina,email} = req.body;
-    const professor = new Professor(nome,disciplina,email);
-    this.conecta
-      .insereProf(professor)
-      .then((resolve) => {
-        res.send({
-          message: "Professor Salvo no banco",
-          data: resolve,
-        });
-      })
-      .catch((err) => {
-        throw err;
-      });
-  };
-
-  //Insira a função que atualiza um professor (método PUT)
-  atualiza = (req, res) => {
+  //Função que atualiza um professor (método PUT)
+  atualiza = async (req, res) => {
     const nomeProf = req.params.nome;
     const { nome, disciplina, email } = req.body;
     const professor = new Professor(nome, disciplina, email);
 
-    this.conecta
-      .atualizaProf(professor, nomeProf)
-      .then((err) => {
-        throw err;
-      })
-      .catch(() => {
-        res.send({ message: "Professor atualizado" });
-        console.log(professor)
-      });
+    try {
+      const atualizaProfe = await this.conecta.atualizaProf(
+        professor,
+        nomeProf
+      );
+      res.send(atualizaProfe);
+    } catch (err) {
+      res.send(err);
+    }
   };
-
-  //Insira a função que exclui um professor (método DELETE)
-
-  remove = (req, res) => {
+  //Função que exclui um professor (método DELETE)
+  remove = async (req, res) => {
     const nome = req.params.nome;
 
-    this.conecta
-      .removeProf(nome)
-      .then((resolve) => {
-        if (resolve) {
-          res.send({
-            message: "Usuário removido do banco de dados",
-            data: nome,
-          });
-        }
-      })
-      .catch((err) => {
-        throw err;
-      });
+    try {
+      const removeProfe = await this.conecta.removeProf(nome);
+      res.send(removeProfe);
+    } catch (err) {
+      res.send(err);
+    }
   };
 }
+
 module.exports = ProfessorController;
+
